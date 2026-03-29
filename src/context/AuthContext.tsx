6b328f4a5867485fd48c18591c341d5d6f6d5006
Whiz7z <gjevstratov@gmail.com>
2026-03-29 19:17:18 +0100

    Update Google Ads conversion reporting in AuthContext and Menu component
    - Modified the `gtag_report_conversion` call in AuthContext to include the session ID for improved tracking.
    - Removed the `gtag_report_conversion` call from the `h1` element in Menu, streamlining the event handling for better clarity.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -65,7 +65,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
           }
           if (verified) {
             console.log("verified", sid);
-            window.gtag_report_conversion!(undefined)
+            window.gtag_report_conversion!(undefined, sid)
           }
           else if (import.meta.env.DEV) {
             console.warn(
